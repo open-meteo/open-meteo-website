5bf5f3a048d0f142e8090e14c69869aaa3cb0742
patrick-zippenfenig <patrick@zippenfenig.de>
2023-04-12 16:43:23 +0200

    refactor geocoding using await promise syntax

--- src/routes/en/docs/geocoding-api/+page.svelte
+++ src/routes/en/docs/geocoding-api/+page.svelte
@@ -1,60 +1,55 @@
 <script lang="ts">
-  let searching = false
-  let timeout
-  let form
-  let results = {"results":[
-    {"id":2950159,"name":"Berlin","latitude":52.52437,"longitude":13.41053,"elevation":74.0,"feature_code":"PPLC","country_code":"DE","admin1_id":2950157,"admin3_id":6547383,"admin4_id":6547539,"timezone":"Europe/Berlin","population":3426354,"postcodes":["10967","13347"],"country_id":2921044,"country":"Germany","admin1":"Land Berlin","admin3":"Berlin, Stadt","admin4":"Berlin"},
-    {"id":5083330,"name":"Berlin","latitude":44.46867,"longitude":-71.18508,"elevation":311.0,"feature_code":"PPL","country_code":"US","admin1_id":5090174,"admin2_id":5084973,"admin3_id":5083340,"timezone":"America/New_York","population":9367,"postcodes":["03570"],"country_id":6252001,"country":"United States","admin1":"New Hampshire","admin2":"Coos","admin3":"City of Berlin"},
-    {"id":4500771,"name":"Berlin","latitude":39.79123,"longitude":-74.92905,"elevation":50.0,"feature_code":"PPL","country_code":"US","admin1_id":5101760,"admin2_id":4501019,"admin3_id":4500776,"timezone":"America/New_York","population":7590,"postcodes":["08009"],"country_id":6252001,"country":"United States","admin1":"New Jersey","admin2":"Camden","admin3":"Borough of Berlin"},
-    {"id":5245497,"name":"Berlin","latitude":43.96804,"longitude":-88.94345,"elevation":246.0,"feature_code":"PPL","country_code":"US","admin1_id":5279468,"admin2_id":5255015,"admin3_id":5245510,"timezone":"America/Chicago","population":5420,"postcodes":["54923"],"country_id":6252001,"country":"United States","admin1":"Wisconsin","admin2":"Green Lake","admin3":"City of Berlin"},
-    {"id":4348460,"name":"Berlin","latitude":38.32262,"longitude":-75.21769,"elevation":11.0,"feature_code":"PPL","country_code":"US","admin1_id":4361885,"admin2_id":4374180,"timezone":"America/New_York","population":4529,"postcodes":["21811"],"country_id":6252001,"country":"United States","admin1":"Maryland","admin2":"Worcester"},
-    {"id":4930431,"name":"Berlin","latitude":42.3812,"longitude":-71.63701,"elevation":100.0,"feature_code":"PPL","country_code":"US","admin1_id":6254926,"admin2_id":4956199,"admin3_id":4930436,"timezone":"America/New_York","population":2422,"postcodes":["01503"],"country_id":6252001,"country":"United States","admin1":"Massachusetts","admin2":"Worcester","admin3":"Town of Berlin"},
-    {"id":4556518,"name":"Berlin","latitude":39.92064,"longitude":-78.9578,"elevation":710.0,"feature_code":"PPL","country_code":"US","admin1_id":6254927,"admin2_id":5212857,"admin3_id":4556520,"timezone":"America/New_York","population":2019,"postcodes":["15530"],"country_id":6252001,"country":"United States","admin1":"Pennsylvania","admin2":"Somerset","admin3":"Borough of Berlin"},
-    {"id":4557666,"name":"East Berlin","latitude":39.9376,"longitude":-76.97859,"elevation":131.0,"feature_code":"PPL","country_code":"US","admin1_id":6254927,"admin2_id":4556228,"admin3_id":4557667,"timezone":"America/New_York","population":1534,"postcodes":["17316"],"country_id":6252001,"country":"United States","admin1":"Pennsylvania","admin2":"Adams","admin3":"Borough of East Berlin"},
-    {"id":5147132,"name":"Berlin","latitude":40.56117,"longitude":-81.7943,"elevation":391.0,"feature_code":"PPL","country_code":"US","admin1_id":5165418,"admin2_id":5157783,"admin3_id":5147154,"timezone":"America/New_York","population":898,"postcodes":["44610"],"country_id":6252001,"country":"United States","admin1":"Ohio","admin2":"Holmes","admin3":"Berlin Township"},
-    {"id":1510159,"name":"Berlin","latitude":54.00603,"longitude":61.19308,"elevation":228.0,"feature_code":"PPL","country_code":"RU","admin1_id":1508290,"admin2_id":1489213,"timezone":"Asia/Yekaterinburg","population":613,"postcodes":["457130"],"country_id":2017370,"country":"Russia","admin1":"Chelyabinsk","admin2":"Troitskiy Rayon"}
-  ], "generationtime_ms":1.0390282}
-
+  let timeout: number | undefined
+  let form: any | undefined
+  let results = getData()
   let apiUrl = "https://geocoding-api.open-meteo.com/v1/search?name=Berlin&language=en&count=10&format=json"
 
   // Called on form submit
   async function submit() {
-    searching = true
     if (timeout) {
       clearTimeout(timeout)
+    }
+    results = getData()
+  }
+
+  async function getData() {
+    if (!form) {
+      return {"results":[
+        {"id":2950159,"name":"Berlin","latitude":52.52437,"longitude":13.41053,"elevation":74.0,"feature_code":"PPLC","country_code":"DE","admin1_id":2950157,"admin3_id":6547383,"admin4_id":6547539,"timezone":"Europe/Berlin","population":3426354,"postcodes":["10967","13347"],"country_id":2921044,"country":"Germany","admin1":"Land Berlin","admin3":"Berlin, Stadt","admin4":"Berlin"},
+        {"id":5083330,"name":"Berlin","latitude":44.46867,"longitude":-71.18508,"elevation":311.0,"feature_code":"PPL","country_code":"US","admin1_id":5090174,"admin2_id":5084973,"admin3_id":5083340,"timezone":"America/New_York","population":9367,"postcodes":["03570"],"country_id":6252001,"country":"United States","admin1":"New Hampshire","admin2":"Coos","admin3":"City of Berlin"},
+        {"id":4500771,"name":"Berlin","latitude":39.79123,"longitude":-74.92905,"elevation":50.0,"feature_code":"PPL","country_code":"US","admin1_id":5101760,"admin2_id":4501019,"admin3_id":4500776,"timezone":"America/New_York","population":7590,"postcodes":["08009"],"country_id":6252001,"country":"United States","admin1":"New Jersey","admin2":"Camden","admin3":"Borough of Berlin"},
+        {"id":5245497,"name":"Berlin","latitude":43.96804,"longitude":-88.94345,"elevation":246.0,"feature_code":"PPL","country_code":"US","admin1_id":5279468,"admin2_id":5255015,"admin3_id":5245510,"timezone":"America/Chicago","population":5420,"postcodes":["54923"],"country_id":6252001,"country":"United States","admin1":"Wisconsin","admin2":"Green Lake","admin3":"City of Berlin"},
+        {"id":4348460,"name":"Berlin","latitude":38.32262,"longitude":-75.21769,"elevation":11.0,"feature_code":"PPL","country_code":"US","admin1_id":4361885,"admin2_id":4374180,"timezone":"America/New_York","population":4529,"postcodes":["21811"],"country_id":6252001,"country":"United States","admin1":"Maryland","admin2":"Worcester"},
+        {"id":4930431,"name":"Berlin","latitude":42.3812,"longitude":-71.63701,"elevation":100.0,"feature_code":"PPL","country_code":"US","admin1_id":6254926,"admin2_id":4956199,"admin3_id":4930436,"timezone":"America/New_York","population":2422,"postcodes":["01503"],"country_id":6252001,"country":"United States","admin1":"Massachusetts","admin2":"Worcester","admin3":"Town of Berlin"},
+        {"id":4556518,"name":"Berlin","latitude":39.92064,"longitude":-78.9578,"elevation":710.0,"feature_code":"PPL","country_code":"US","admin1_id":6254927,"admin2_id":5212857,"admin3_id":4556520,"timezone":"America/New_York","population":2019,"postcodes":["15530"],"country_id":6252001,"country":"United States","admin1":"Pennsylvania","admin2":"Somerset","admin3":"Borough of Berlin"},
+        {"id":4557666,"name":"East Berlin","latitude":39.9376,"longitude":-76.97859,"elevation":131.0,"feature_code":"PPL","country_code":"US","admin1_id":6254927,"admin2_id":4556228,"admin3_id":4557667,"timezone":"America/New_York","population":1534,"postcodes":["17316"],"country_id":6252001,"country":"United States","admin1":"Pennsylvania","admin2":"Adams","admin3":"Borough of East Berlin"},
+        {"id":5147132,"name":"Berlin","latitude":40.56117,"longitude":-81.7943,"elevation":391.0,"feature_code":"PPL","country_code":"US","admin1_id":5165418,"admin2_id":5157783,"admin3_id":5147154,"timezone":"America/New_York","population":898,"postcodes":["44610"],"country_id":6252001,"country":"United States","admin1":"Ohio","admin2":"Holmes","admin3":"Berlin Township"},
+        {"id":1510159,"name":"Berlin","latitude":54.00603,"longitude":61.19308,"elevation":228.0,"feature_code":"PPL","country_code":"RU","admin1_id":1508290,"admin2_id":1489213,"timezone":"Asia/Yekaterinburg","population":613,"postcodes":["457130"],"country_id":2017370,"country":"Russia","admin1":"Chelyabinsk","admin2":"Troitskiy Rayon"}
+      ], "generationtime_ms":1.0390282}
     }
     const action = form.action
     const formData = new FormData(form)
     const data = new URLSearchParams()
     for (let field of formData) {
       const [key, value] = field
-      data.append(key, value)
+      data.append(key, value.toString())
     }
     apiUrl = `${action}?${data}`
 
-    results = await fetch(`${apiUrl}&format=json`).then((res) => {
-      if (!res.ok) {
-        handleError()
-        return {}
-      }
-      return res.json()
-    }).catch((e) => handleError())
-    searching = false
+    const res = await fetch(`${apiUrl}&format=json`)
+
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return await res.json()
   }
 
   // Schedule a form submit in 300 ms
   async function debounceSubmit() {
-    searching = true
     if (timeout) {
       clearTimeout(timeout)
     }
     timeout = setTimeout(submit, 300)
-  }
-  
-  function handleError() {
-    alert('Something went wrong.')
-    results = {}
-    searching = false
   }
 </script>
 
@@ -72,13 +67,6 @@
         <div class="form-floating position-relative">
           <input type="text" class="form-control" name="name" id="name" value="Berlin" on:input={debounceSubmit}>
           <label for="name">Name</label>
-          {#if searching}
-          <div class="position-absolute top-0 end-0 py-4 px-2">
-            <div class="spinner-border spinner-border-sm" role="status">
-              <span class="visually-hidden">Loading...</span>
-            </div>
-          </div>
-          {/if}
         </div>
       </div>
       <div class="col-3">
@@ -121,6 +109,14 @@
     </div>
   </form>
 
+  {#await results}
+  <div class="col-12">
+    <button class="btn btn-primary" type="button" disabled>
+      <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+      Loading...
+    </button>
+  </div>
+  {:then results}
   <div class="col-12 table-responsive">
     <table class="table">
       <thead>
@@ -163,6 +159,13 @@
       </tbody>
     </table>
   </div>
+  {:catch error}
+  <div class="col-12">
+    <div class="alert alert-danger" role="alert">
+      {error.message}
+    </div>
+  </div>
+  {/await}
 
   <div class="col-12">
     <label for="api_url" class="form-label">API URL (<a id="api_url_link" target="_blank" href="{apiUrl}">Open in new
