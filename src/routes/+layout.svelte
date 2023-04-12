9702a0eff17ea65c529b4e8c80293424e19363fa
patrick-zippenfenig <patrick@zippenfenig.de>
2023-04-12 10:55:10 +0200

    remove border on footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -116,7 +116,7 @@
 
   <slot />
 
-  <footer class="container py-5 pt-4 my-md-5 pt-md-5 border-top">
+  <footer class="container py-5 pt-4 my-md-5 pt-md-5">
     <div class="row">
       <div class="col-6 col-md">
         <p class="h5">Features</p>
