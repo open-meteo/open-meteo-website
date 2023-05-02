473c168001f4d0185b796eab313c2bee1b68fb97
patrick-zippenfenig <patrick@zippenfenig.de>
2023-05-02 16:11:45 +0200

    ensemble api docs

--- src/routes/en/docs/ensemble-api/+page.ts
+++ src/routes/en/docs/ensemble-api/+page.ts
@@ -3,7 +3,7 @@ import type { PageLoad } from './$types';
 export const load = (({ params }) => {
   return {
     title: "Ensemble API",
-    subtitle: "",
-    hero: "/images/partly_cloudy.webp"
+    subtitle: "Perturbed forecasts for probabilistic weather prediction",
+    hero: "/images/rocky_coast.webp"
   };
 }) satisfies PageLoad;
