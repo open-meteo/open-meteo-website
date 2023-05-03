c81c5e21ccab238d00347b8166d7c54ef6a43f79
patrick-zippenfenig <patrick@zippenfenig.de>
2023-05-03 19:05:49 +0200

    new license, terms, privacy and about pages

---
+++ src/routes/en/terms/+page.svelte
@@ -0,0 +1,100 @@
+<svelte:head>
+	<title>Terms | Open-Meteo.com</title>
+	<link rel="canonical" href="https://open-meteo.com/en/terms" />
+</svelte:head>
+
+<div
+	class="px-4 py-5 text-center text-white"
+	style="
+            background-image: url('/images/features_background.webp');
+            background-size: cover;
+            background-position: center;
+            height: 300px;
+          "
+>
+	<svg
+		style="filter: drop-shadow( 3px 3px 2px rgba(0, 0, 0, .7));"
+		xmlns="http://www.w3.org/2000/svg"
+		width="96"
+		height="96"
+		fill="currentColor"
+		class="bi bi-cloud-rain mb-4"
+		viewBox="0 0 16 16"
+	>
+		<path
+			d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm3 0a.5.5 0 0 1 .316.633l-1 3a.5.5 0 0 1-.948-.316l1-3a.5.5 0 0 1 .632-.317zm3 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm3 0a.5.5 0 0 1 .316.633l-1 3a.5.5 0 1 1-.948-.316l1-3a.5.5 0 0 1 .632-.317zm.247-6.998a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973zM8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 2z"
+		/>
+	</svg>
+	<h1 class="display-5" style="text-shadow: 3px 3px 2px rgba(0, 0, 0, .7);">Terms & Privacy</h1>
+	<div class="col-lg-6 mx-auto" />
+</div>
+
+<div class="container col-xxl-8 px-4 py-5">
+	<div class="row flex-lg-row align-items-top g-5 py-5">
+		<div class="col-lg-6">
+			<h2 id="terms" class="display-5 lh-1 mb-3">Terms of Use</h2>
+			<p class="lead">
+				Open-Meteo APIs are free for non-commercial use. We do not restrict access, but ask for fair
+				use.
+			</p>
+			<p>
+				If your application exceeds 10'000 requests per day, please contact us. We reserve the right
+				to block applications and IP addresses that misuse our service.
+			</p>
+			<p>
+				<strong>Free for non-commercial:</strong> You may only use the API services for free for
+				non-commercial purposes as
+				<a href="https://wiki.creativecommons.org/wiki/NonCommercial_interpretation"
+					>elaborated by creative commons</a
+				>. For further questions, please contact us.
+			</p>
+			<p>
+				Open-Meteo assumes no responsibility for any inaccuracies or omissions in the data, their
+				availability, or any losses or damages resulting from their usage. The accuracy and
+				completeness of the data products and their uninterrupted provision are not guaranteed by
+				Open-Meteo, and they are provided without any warranty, either statutory or otherwise. In
+				any event, Open-Meteo disclaims all liability under these Terms of Use, whether arising from
+				contract, tort, or any other cause.
+			</p>
+			<p class="lead">Open-Meteo is operated by:</p>
+			<p>
+				Patrick Zippenfenig<br />
+				Hintere Schilligmatte 6<br />
+				6463 Bürglen (UR)<br />
+				Switzerland
+			</p>
+			<p>Contact: <a href="mailto:info@open-meteo.com">info@open-meteo.com</a></p>
+		</div>
+		<div class="col-lg-6">
+			<h2 id="terms" class="display-5 lh-1 mb-3">Privacy</h2>
+			<p class="lead">
+				We do not collect any data from our users, including personal information, usage statistics,
+				or any other data that could be used to identify you. We do not use any cookies or tracking
+				technologies, and we do not share any data with third-party providers.
+			</p>
+			<p>
+				We do not engage in any advertising or marketing practices that would require us to collect
+				or use any personal information. You can use our website without any concern that your
+				information will be shared with advertisers or marketing agencies.
+			</p>
+			<p>
+				When you sign up for our newsletter, we collect your email address. We use a secure
+				third-party email marketing platform <a
+					href="https://substack.com/privacy"
+					title="Substack privacy policy">Substack.com</a
+				> to manage our newsletter. This platform collects information such as your email address and
+				IP address. We use this information solely for the purpose of sending you our newsletter and
+				will not share or sell it with any third-party providers.
+			</p>
+			<p>
+				We may collect non-personal information, such as your IP address, for technical reasons such
+				as server maintenance, but this information is not linked to your identity in any way.
+			</p>
+			<p>
+				By using our service, you consent to the terms of our privacy policy and agree that we may
+				collect non-personal information for technical reasons. If you have any questions or
+				concerns about our privacy policy, please contact us.
+			</p>
+		</div>
+	</div>
+</div>
