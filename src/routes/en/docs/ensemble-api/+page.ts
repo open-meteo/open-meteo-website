f2abaf194db5616e73b8a09c17dfda523cafba28
patrick-zippenfenig <patrick@zippenfenig.de>
2023-06-05 16:05:10 +0200

    ensemble api slogan

--- src/routes/en/docs/ensemble-api/+page.ts
+++ src/routes/en/docs/ensemble-api/+page.ts
@@ -3,7 +3,7 @@ import type { PageLoad } from './$types';
 export const load = (({ params }) => {
   return {
     title: "Ensemble API",
-    subtitle: "Perturbed forecasts for probabilistic weather prediction",
+    subtitle: "Hundreds Of Weather Forecasts, Every time, Everywhere, All at Once",
     hero: "/images/rocky_coast.webp"
   };
 }) satisfies PageLoad;
