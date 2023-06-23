e1e13176ccc17b57415f6510b4046db66b30f40a
patrick-zippenfenig <patrick@zippenfenig.de>
2023-06-24 00:53:34 +0200

    highchart container

---
+++ src/lib/HighchartContainer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Highcharts, { Chart, StockChart, type Options } from 'highcharts/highstock';
+	import { onMount } from 'svelte';
+	import HighchartsAccessibility from 'highcharts/modules/accessibility';
+	import { dev } from '$app/environment';
+	//import HighchartsBoost from "highcharts/modules/boost";
+	//import { themeIsDark } from '$lib/stores';
+
+	export let useStockChart = false;
+	export let options: Options
+	export let style = "height: 400px"
+	let clazz = "w-100";
+	export { clazz as class };
+
+	let node: HTMLElement;
+	let chart: Chart;
+
+	onMount(async () => {
+		if (dev) {
+			const HighchartsDebugger = await import('highcharts/modules/debugger');
+			HighchartsDebugger.default(Highcharts);
+		}
+		HighchartsAccessibility(Highcharts);
+		chart = useStockChart
+			? new StockChart(node, options)
+			: new Chart(node, options);
+	});
+</script>
+
+<div bind:this={node} style={style} class={clazz} />
--- src/routes/en/docs/ResultPreview.svelte
+++ src/routes/en/docs/ResultPreview.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { api_key_preferences } from '$lib/stores';
 	import type { Writable } from 'svelte/store';
+	import HighchartContainer from '$lib/HighchartContainer.svelte';
 
 	export let params: Writable<any>;
 	export let type: String | undefined;
@@ -16,6 +17,86 @@
 		}
 		return diff;
 	}
+
+    let options = {
+		title: {
+			text: 'U.S Solar Employment Growth',
+			align: 'left'
+		},
+
+		subtitle: {
+			text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
+			align: 'left'
+		},
+
+		yAxis: {
+			title: {
+				text: 'Number of Employees'
+			}
+		},
+
+		xAxis: {
+			accessibility: {
+				rangeDescription: 'Range: 2010 to 2020'
+			}
+		},
+
+		legend: {
+			layout: 'vertical',
+			align: 'right',
+			verticalAlign: 'middle'
+		},
+
+		plotOptions: {
+			series: {
+				label: {
+					connectorAllowed: false
+				},
+				pointStart: 2010
+			}
+		},
+
+		series: [
+			{
+				name: 'Installation & Developers',
+				data: [43934, 48656, 65165, 81827, 112143, 142383, 171533, 165174, 155157, 161454, 154610]
+			},
+			{
+				name: 'Manufacturing',
+				data: [24916, 37941, 29742, 29851, 32490, 30282, 38121, 36885, 33726, 34243, 31050]
+			},
+			{
+				name: 'Sales & Distribution',
+				data: [11744, 30000, 16005, 19771, 20185, 24377, 32147, 30912, 29243, 29213, 25663]
+			},
+			{
+				name: 'Operations & Maintenance',
+				data: [null, null, null, null, null, null, null, null, 11164, 11218, 10077]
+			},
+			{
+				name: 'Other',
+				data: [21908, 5548, 8105, 11248, 8989, 11816, 18274, 17300, 13053, 11906, 10073]
+			}
+		],
+
+		responsive: {
+			rules: [
+				{
+					condition: {
+						maxWidth: 500
+					},
+					chartOptions: {
+						legend: {
+							layout: 'horizontal',
+							align: 'center',
+							verticalAlign: 'bottom'
+						}
+					}
+				}
+			]
+		}
+	};
+
 
 	let action = `https://api.open-meteo.com/v1/forecast?`;
     let actionParams = {}
@@ -38,7 +119,14 @@
 
 <div class="col-12 my-4">
 	<h2>Preview and API URL</h2>
-	<div id="container" style="height: 400px; width: 100%" />
+    <HighchartContainer options={options}/>
+	<div id="container" style="height: 400px; width: 100%">
+        <div class="h-100 d-flex justify-content-center align-items-center">
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="col-12">
 	<button type="submit" class="btn btn-primary">Preview Chart</button>
