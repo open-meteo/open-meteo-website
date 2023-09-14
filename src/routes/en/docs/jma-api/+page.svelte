18d03bcf7e8de1e51a7968b1c116815619baca53
patrick-zippenfenig <patrick@zippenfenig.de>
2023-09-14 14:05:27 +0200

    update texts + icons

--- src/routes/en/docs/jma-api/+page.svelte
+++ src/routes/en/docs/jma-api/+page.svelte
@@ -13,7 +13,7 @@
 	} from '$lib/meteo';
 	import AccordionItem from '$lib/Elements/AccordionItem.svelte';
 	import { slide, fade } from 'svelte/transition';
-	import { PlusLg, Trash } from 'svelte-bootstrap-icons';
+	import { CalendarEvent, Clock, PlusLg, Trash } from 'svelte-bootstrap-icons';
 	import StartEndDate from '../StartEndDate.svelte';
 
 	const defaultParameter = {
@@ -223,7 +223,7 @@
 						role="tab"
 						aria-controls="pills-forecast_days"
 						aria-selected="true"
-						on:click={() => ($params.time_mode = 'forecast_days')}>Forecast Length</button
+						on:click={() => ($params.time_mode = 'forecast_days')}><Clock/> Forecast Length</button
 					>
 				</li>
 				<li class="nav-item" role="presentation">
@@ -235,7 +235,7 @@
 						role="tab"
 						aria-controls="pills-time_interval"
 						on:click={() => ($params.time_mode = 'time_interval')}
-						aria-selected="true">Time Interval</button
+						aria-selected="true"><CalendarEvent/> Time Interval</button
 					>
 				</li>
 			</ul>
