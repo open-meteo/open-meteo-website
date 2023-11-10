2c71a14b9aecc5f3b84d7566558899d62602694d
patrick-zippenfenig <patrick@zippenfenig.de>
2023-11-10 17:32:52 +0100

    fix: optimise highcharts bundling

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// Make sure highcharts and highstock are bundled separately
+function manualChunks(id) {
+	//return `${id}`.replace('/','_');
+	if (id.includes('stock.js')) {
+		return 'highstock'
+	}
+	if (id.includes('highcharts')) {
+		return 'highcharts'
+	}
+	/*if (id.includes('node_modules')) {
+		return 'vendor';
+	}*/
+}
+
 export default defineConfig({
 	plugins: [sveltekit()],
 
@@ -8,6 +22,13 @@ export default defineConfig({
 		preprocessorOptions: {
 			scss: {
 				additionalData: '@use "src/variables.scss" as *;'
+			}
+		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: manualChunks
 			}
 		}
 	}
