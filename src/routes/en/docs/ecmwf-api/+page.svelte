0172225e2ac30ede1e9aa40502466b40252fb25c
Patrick Zippenfenig <patrick@zippenfenig.de>
2024-11-12 22:52:28 +0100

    feat: add ECMWF IFS0.25 wind gusts (#342)

--- src/routes/en/docs/ecmwf-api/+page.svelte
+++ src/routes/en/docs/ecmwf-api/+page.svelte
@@ -81,6 +81,7 @@
 			{ name: 'wind_speed_100m', label: 'Wind Speed (100 m)' },
 			{ name: 'wind_direction_10m', label: 'Wind Direction (10 m)' },
 			{ name: 'wind_direction_100m', label: 'Wind Direction (100 m)' },
+			{ name: 'wind_gusts_10m', label: 'Wind Gusts (10 m)' },
 			{ name: 'surface_temperature', label: 'Surface temperature' },
 			{ name: 'runoff', label: 'Surface Water Runoff' },
 			{ name: 'cape', label: 'CAPE' },
