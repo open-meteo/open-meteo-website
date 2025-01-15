8509acf5b4c29b3d0032e9689bc8d16682ae9bcf
Vincent van der Wal <info@vincentvanderwal.nl>
2025-01-15 16:28:32 +0100

    update gitignore and vite rollup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		visualizer({filename: "build-stats.html"}),
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
