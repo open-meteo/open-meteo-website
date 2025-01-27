1b8c097fb63d94df6e9f815e2070e5505dc1496e
Vincent van der Wal <111272374+vincvdwal@users.noreply.github.com>
2025-01-27 17:48:31 +0100

    feat: Switch to X logo (#382)
    Co-authored-by: Vincent van der Wal <info@vincentvanderwal.nl>

---
+++ src/lib/components/footer/footer.svelte
@@ -0,0 +1,150 @@
+<footer class="container py-5 pt-4 my-md-5 pt-md-5">
+	<div class="row">
+		<div class="col-6 col-md">
+			<p class="h5">Open-Meteo</p>
+			<ul class="list-unstyled text-small">
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/features">Features</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/pricing">Pricing</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/about">About us &amp; Contact</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/license">License</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/terms">Terms &amp; Privacy</a>
+				</li>
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<p class="h5">Weather APIs</p>
+			<ul class="list-unstyled text-small">
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs">Weather Forecast API</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/historical-weather-api"
+						>Historical Weather API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/ecmwf-api">ECMWF API</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/gfs-api"
+						>GFS & HRRR Forecast API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/meteofrance-api"
+						>Météo-France API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/dwd-api">DWD ICON API</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/gem-api">GEM API</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/jma-api">JMA API</a>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/metno-api">Met Norway API</a
+					>
+				</li>
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<p class="h5">Other APIs</p>
+			<ul class="list-unstyled text-small">
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/ensemble-api"
+						>Ensemble API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/climate-api"
+						>Climate Change API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/marine-weather-api"
+						>Marine Weather API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/air-quality-api"
+						>Air Quality API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/geocoding-api"
+						>Geocoding API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/elevation-api"
+						>Elevation API</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/flood-api">Flood API</a>
+				</li>
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<p class="h5">External</p>
+			<ul class="list-unstyled text-small">
+				<li class="mb-1">
+					<a
+						rel="me"
+						class="link-secondary text-decoration-none pe-4"
+						href="https://x.com/open_meteo"
+						target="_blank">X</a
+					>
+				</li>
+				<li class="mb-1">
+					<a
+						class="link-secondary text-decoration-none"
+						href="https://openmeteo.substack.com/archive?sort=new">Blog</a
+					>
+				</li>
+				<li class="mb-1">
+					<a
+						class="link-secondary text-decoration-none"
+						href="https://github.com/open-meteo/open-meteo">GitHub</a
+					>
+				</li>
+
+				<li class="mb-1">
+					<a
+						rel="me"
+						class="link-secondary text-decoration-none"
+						href="https://fosstodon.org/@openmeteo"
+						target="_blank">Mastodon</a
+					>
+				</li>
+				<li class="mb-1">
+					<a class="link-secondary text-decoration-none" href="/en/docs/model-updates"
+						>Model Updates Overview</a
+					>
+				</li>
+				<li class="mb-1">
+					<a
+						class="link-secondary text-decoration-none"
+						href="https://status.open-meteo.com"
+						target="_blank">Service status and uptime</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="text-center pt-3">
+		<small class="d-block text-muted">© 2022-2025 Copyright: Open-Meteo.com</small>
+	</div>
+</footer>
