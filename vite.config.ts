67593ea7e7cb213fdb944d7bbfa85b4a872c78d4
Vincent van der Wal <info@vincentvanderwal.nl>
2025-02-05 09:38:46 +0100

    hash strip in compare

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,22 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
+function replaceChunckNames() {
+	return {
+		name: 'replace-chunk-names-plugin',
+		apply: 'build' as const,
+		config(config) {
+			config.build.rollupOptions.output.chunkFileNames =
+				config.build.rollupOptions.output.chunkFileNames.replace('[hash]', `[name].[hash].chunk`);
+			return config;
+		}
+	};
+}
+
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		replaceChunckNames(),
 		visualizer({
 			filename: 'build-stats.json',
 			template: 'raw-data'
