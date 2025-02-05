d7836296aa5855c3857945308e1da197bf111544
Vincent van der Wal <111272374+vincvdwal@users.noreply.github.com>
2025-02-05 10:32:12 +0100

    feat: Use PurgeCSS to purge dem bootstrap (#391)
    Co-authored-by: Vincent van der Wal <info@vincentvanderwal.nl>

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
+import { purgeCss } from './src/lib/purge/index';
+
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		purgeCss({
+			safelist: {
+				greedy: [/highcharts/, /svelte-ergyxs/, /sdt-/, /dropdown-/]
+			}
+		}),
 		visualizer({
 			filename: 'build-stats.json',
 			template: 'raw-data'
