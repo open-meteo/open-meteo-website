c2929ceba4108cbf5a39d7a7cd8bdcde4735d550
Vincent van der Wal <info@vincentvanderwal.nl>
2025-04-25 15:40:04 +0200

    switch to other mailbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,6 @@
 
 	import Github from 'simple-icons/icons/github.svg?raw';
 
-	import Mailbox from '$lib/assets/icons/mailbox.svelte';
-
 	import { Button } from '$lib/components/ui/button';
 
 	import * as ToggleGroup from '$lib/components/ui/toggle-group/index.js';
@@ -414,8 +412,24 @@
 	</div>
 
 	<div class="my-12 mb-24 flex flex-col items-center justify-center">
-		<Mailbox width="72" height="72" shadow={false} />
-		<h2 class="mb-4 mt-2 text-4xl font-bold" id="contact">Stay up to date!</h2>
+		<svg
+			class="lucide lucide-mailbox"
+			xmlns="http://www.w3.org/2000/svg"
+			width="72"
+			height="72"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="1.35"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path d="M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9.5C2 7 4 5 6.5 5H18c2.2 0 4 1.8 4 4v8Z" />
+			<polyline points="15,9 18,9 18,11" />
+			<path d="M6.5 5C9 5 11 7 11 9.5V17a2 2 0 0 1-2 2" />
+			<line x1="6" x2="7" y1="10" y2="10" />
+		</svg>
+		<h2 class="mb-4 text-4xl font-bold" id="contact">Stay up to date!</h2>
 		<div class="text-center">
 			<p>
 				We're constantly evolving and expanding. We're dedicated to providing you with the <a
--- src/routes/en/features/+page.svelte
+++ src/routes/en/features/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Mailbox from '$lib/assets/icons/mailbox.svelte';
-
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
@@ -557,8 +555,24 @@
 	</div>
 
 	<div class="my-12 mb-24 flex flex-col items-center justify-center">
-		<Mailbox width="72" height="72" shadow={false} />
-		<h2 class="my-4 text-4xl font-bold" id="contact">Next steps</h2>
+		<svg
+			class="lucide lucide-mailbox"
+			xmlns="http://www.w3.org/2000/svg"
+			width="72"
+			height="72"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="1.35"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path d="M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9.5C2 7 4 5 6.5 5H18c2.2 0 4 1.8 4 4v8Z" />
+			<polyline points="15,9 18,9 18,11" />
+			<path d="M6.5 5C9 5 11 7 11 9.5V17a2 2 0 0 1-2 2" />
+			<line x1="6" x2="7" y1="10" y2="10" />
+		</svg>
+		<h2 class="mb-4 text-4xl font-bold" id="contact">Next steps</h2>
 		<div class="text-center">
 			<p class="lead mb-4">
 				Our <a href="/en/docs">documentation</a> provides a good start to help you integrate weather
