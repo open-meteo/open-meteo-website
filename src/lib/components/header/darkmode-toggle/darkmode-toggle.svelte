639c72964416f024c1c9c4845c4f156627199044
Vincent van der Wal <111272374+vincvdwal@users.noreply.github.com>
2025-05-06 12:44:53 +0200

    fix: Remove animation Darkmode and use opacity in css (#584)
    Co-authored-by: Vincent van der Wal <info@vincentvanderwal.nl>

--- src/lib/components/header/darkmode-toggle/darkmode-toggle.svelte
+++ src/lib/components/header/darkmode-toggle/darkmode-toggle.svelte
@@ -10,45 +10,42 @@
 		aria-labelledby="darkmode_toggle_label"
 	>
 		<span id="darkmode_toggle_label" class="md:hidden">Toggle theme</span>
-		{#if mode.current === 'dark'}
-			<svg
-				class="lucide lucide-moon"
-				xmlns="http://www.w3.org/2000/svg"
-				width="22"
-				height="22"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-			</svg>
-		{:else}
-			<svg
-				class="lucide lucide-sun"
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<circle cx="12" cy="12" r="4" />
-				<path d="M12 2v2" />
-				<path d="M12 20v2" />
-				<path d="m4.93 4.93 1.41 1.41" />
-				<path d="m17.66 17.66 1.41 1.41" />
-				<path d="M2 12h2" />
-				<path d="M20 12h2" />
-				<path d="m6.34 17.66-1.41 1.41" />
-				<path d="m19.07 4.93-1.41 1.41" />
-			</svg>
-		{/if}
+		<svg
+			class="darkmode-icon-moon lucide lucide-moon"
+			xmlns="http://www.w3.org/2000/svg"
+			width="22"
+			height="22"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="1.5"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+		</svg>
+		<svg
+			class="darkmode-icon-sun lucide lucide-sun"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="1.5"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<circle cx="12" cy="12" r="4" />
+			<path d="M12 2v2" />
+			<path d="M12 20v2" />
+			<path d="m4.93 4.93 1.41 1.41" />
+			<path d="m17.66 17.66 1.41 1.41" />
+			<path d="M2 12h2" />
+			<path d="M20 12h2" />
+			<path d="m6.34 17.66-1.41 1.41" />
+			<path d="m19.07 4.93-1.41 1.41" />
+		</svg>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content preventScroll={false} align="end" class="border-border p-0">
 		<DropdownMenu.Item class="cursor-pointer px-3 py-2" onclick={() => setMode('light')}>
