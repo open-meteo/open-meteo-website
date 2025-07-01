aa2320a050c6649f1f81abbe386542bbc53a1bf4
Vincent van der Wal <info@vincentvanderwal.nl>
2025-07-01 12:18:40 +0200

    split test files and adjust paths

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,12 @@
+export const titleCase = (s: string) =>
+	s.replace(/^[-_]*(.)/, (_, c) => c.toUpperCase()).replace(/[-_]+(.)/g, (_, c) => c.toUpperCase());
+
+export const camelCase = (s: string) => s.replace(/[-_]+(.)/g, (_, c) => c.toUpperCase());
+
+export const isNumeric = (num: string | number) =>
+	(typeof num === 'number' || (typeof num === 'string' && num.trim() !== '')) &&
+	!isNaN(num as number);
+
 export const pad = (n: string | number) => {
 	if (n === null || n === undefined) {
 		return '';
@@ -15,10 +24,6 @@ export function debounce(func, timeout = 100) {
 	};
 }
 
-export const isNumeric = (num: string | number) =>
-	(typeof num === 'number' || (typeof num === 'string' && num.trim() !== '')) &&
-	!isNaN(num as number);
-
 // Only considers keys of the first object. Ignores nulls and empty strings
 export function objectDifference<T extends Record<string, any>>(a: T, b: T): Partial<T> {
 	const diff: Partial<T> = {};
@@ -29,12 +34,6 @@ export function objectDifference<T extends Record<string, any>>(a: T, b: T): Par
 	}
 	return diff;
 }
-
-/// `temperature_2m` to `Temperature2m`
-export const titleCase = (s: string) =>
-	s.replace(/^[-_]*(.)/, (_, c) => c.toUpperCase()).replace(/[-_]+(.)/g, (_, c) => c.toUpperCase());
-
-export const camelCase = (s: string) => s.replace(/[-_]+(.)/g, (_, c) => c.toUpperCase());
 
 export const sliceIntoChunks = (arr: Array<T>, chunkSize: number): Array<Array<T>> => {
 	const res = [];
--- src/lib/utils/meteo.test.ts
+++ src/lib/utils/meteo.test.ts
@@ -1,149 +1,71 @@
 import { expect, test } from 'vitest';
-import * as meteo from './meteo';
-
-// --- pad ---
-test('check pad with 1 digit', () => {
-	expect(meteo.pad(1)).toBe('01');
-});
-
-test('check pad with 2 digit', () => {
-	expect(meteo.pad(10)).toBe('10');
-});
-
-test('check pad with 1 char string', () => {
-	expect(meteo.pad('7')).toBe('07');
-});
-
-test('check pad with 2 char strings', () => {
-	expect(meteo.pad('70')).toBe('70');
-});
-
-test('check pad with overflow string', () => {
-	expect(meteo.pad('18276')).toBe('76');
-});
-
-test('check pad with null', () => {
-	expect(meteo.pad(null)).toBe('');
-});
-
-test('check pad with undefined', () => {
-	expect(meteo.pad(undefined)).toBe('');
-});
-
-// --- isNumeric ---
-test('check isNumeric for small number', () => {
-	expect(meteo.isNumeric(5)).toBe(true);
-});
-
-test('check isNumeric for a negative number', () => {
-	expect(meteo.isNumeric(-55)).toBe(true);
-});
-
-test('check isNumeric for a floating point number', () => {
-	expect(meteo.isNumeric(5.55)).toBe(true);
-});
-
-test('check isNumeric for a bigger number', () => {
-	expect(meteo.isNumeric(23598)).toBe(true);
-});
-
-test('check isNumeric for a biggest numbers', () => {
-	expect(meteo.isNumeric(1e21)).toBe(true);
-});
-
-test('check isNumeric for small number string', () => {
-	expect(meteo.isNumeric('3')).toBe(true);
-});
-
-test('check isNumeric for bigger number string', () => {
-	expect(meteo.isNumeric('153610')).toBe(true);
-});
-
-test('check isNumeric with a trailing 0', () => {
-	expect(meteo.isNumeric('017')).toBe(true);
-});
-
-test('check isNumeric with character in string', () => {
-	expect(meteo.isNumeric('01n7')).toBe(false);
-});
-
-test('check isNumeric with completer character string', () => {
-	expect(meteo.isNumeric('hello')).toBe(false);
-});
-
-test('check isNumeric with null', () => {
-	expect(meteo.isNumeric(null)).toBe(false);
-});
-
-test('check isNumeric with undefined', () => {
-	expect(meteo.isNumeric(undefined)).toBe(false);
-});
+import { getWeatherCode, altitudeAboveSeaLevelMeters, geoLocationNameToRoute } from './meteo';
 
 // --- altitudeAboveSeaLevelMeters ---
 test('check altitudeAboveSeaLevelMeters with 1000hPa', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(1000)).toBe('110 m');
+	expect(altitudeAboveSeaLevelMeters(1000)).toBe('110 m');
 });
 
 test('check altitudeAboveSeaLevelMeters with 750hPa', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(750)).toBe('2.5 km');
+	expect(altitudeAboveSeaLevelMeters(750)).toBe('2.5 km');
 });
 
 test('check altitudeAboveSeaLevelMeters with 500hPa', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(500)).toBe('5.6 km');
+	expect(altitudeAboveSeaLevelMeters(500)).toBe('5.6 km');
 });
 
 test('check altitudeAboveSeaLevelMeters with 250hPa', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(250)).toBe('10.4 km');
+	expect(altitudeAboveSeaLevelMeters(250)).toBe('10.4 km');
 });
 
 test('check altitudeAboveSeaLevelMeters with 0hPa', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(0)).toBe('44 km');
+	expect(altitudeAboveSeaLevelMeters(0)).toBe('44 km');
 });
 
 test('check altitudeAboveSeaLevelMeters with negative pressure', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(-250)).toBe('NaN km');
+	expect(altitudeAboveSeaLevelMeters(-250)).toBe('NaN km');
 });
 
 // appearently function also does below sea level :P
 test('check altitudeAboveSeaLevelMeters with very big postive pressure', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(25000)).toBe('-37250 m');
+	expect(altitudeAboveSeaLevelMeters(25000)).toBe('-37250 m');
 });
 
 test('check altitudeAboveSeaLevelMeters with undefined', () => {
-	expect(meteo.altitudeAboveSeaLevelMeters(undefined)).toBe('NaN km');
+	expect(altitudeAboveSeaLevelMeters(undefined)).toBe('NaN km');
 });
 
 // --- getWeatherCode ---
 
 test('check getWeatherCode for 1', () => {
-	expect(meteo.getWeatherCode(1)).toBe('mainly clear');
+	expect(getWeatherCode(1)).toBe('mainly clear');
 });
 
 test('check getWeatherCode for 82', () => {
-	expect(meteo.getWeatherCode(82)).toBe('heavy rain showers');
+	expect(getWeatherCode(82)).toBe('heavy rain showers');
 });
 
 test('check getWeatherCode unknown weather code', () => {
-	expect(meteo.getWeatherCode(117)).toBe('unknown code');
+	expect(getWeatherCode(117)).toBe('unknown code');
 });
 
 test('check getWeatherCode for null', () => {
-	expect(meteo.getWeatherCode(null)).toBe('-');
+	expect(getWeatherCode(null)).toBe('-');
 });
 
 test('check getWeatherCode for undefined', () => {
-	expect(meteo.getWeatherCode(undefined)).toBe('-');
+	expect(getWeatherCode(undefined)).toBe('-');
 });
 
 // --- geoLocationNameToRoute ---
 test("check geoLocationNameToRoute with 'são-paolo'", () => {
-	expect(meteo.geoLocationNameToRoute('são-paolo')).toBe('sao-paolo');
+	expect(geoLocationNameToRoute('são-paolo')).toBe('sao-paolo');
 });
 
 test("check geoLocationNameToRoute with 'xi'an'", () => {
-	expect(meteo.geoLocationNameToRoute("xi'an")).toBe('xi-an');
+	expect(geoLocationNameToRoute("xi'an")).toBe('xi-an');
 });
 
 test("check geoLocationNameToRoute with 'new taipei city'", () => {
-	expect(meteo.geoLocationNameToRoute('new taipei city')).toBe('new-taipei-city');
+	expect(geoLocationNameToRoute('new taipei city')).toBe('new-taipei-city');
 });
