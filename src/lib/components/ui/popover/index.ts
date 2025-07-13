17f191556afbd28fe7e25773af1ab5ac52f24a68
Vincent van der Wal <info@vincentvanderwal.nl>
2025-07-13 11:33:08 +0200

    switch to date instead of string

--- src/lib/components/ui/popover/index.ts
+++ src/lib/components/ui/popover/index.ts
@@ -1,7 +1,7 @@
 import { Popover as PopoverPrimitive } from 'bits-ui';
 import Content from './popover-content.svelte';
 const Root = PopoverPrimitive.Root;
-const Trigger = './popover-trigger.svelte';
+import Trigger from './popover-trigger.svelte';
 const Close = PopoverPrimitive.Close;
 
 export {
