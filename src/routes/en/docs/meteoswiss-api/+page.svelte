85d2f93a499b7aa8dd3c2c2245aed9e78efae7b8
Vincent van der Wal <info@vincentvanderwal.nl>
2025-07-29 09:20:31 +0200

    change path to results-preview

--- src/routes/en/docs/meteoswiss-api/+page.svelte
+++ src/routes/en/docs/meteoswiss-api/+page.svelte
@@ -18,7 +18,7 @@
 
 	import Settings from '$lib/components/settings/settings.svelte';
 	import DatePicker from '$lib/components/date/date-picker.svelte';
-	import ResultPreview from '$lib/components/highcharts/result-preview.svelte';
+	import ResultPreview from '$lib/components/response/result-preview.svelte';
 	import AccordionItem from '$lib/components/accordion/accordion-item.svelte';
 	import LicenseSelector from '$lib/components/license/license-selector.svelte';
 	import LocationSelection from '$lib/components/location/location-selection.svelte';
