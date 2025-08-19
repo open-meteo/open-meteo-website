9d627ad0db05285775e7e913b388e4f527c38088
Vincent van der Wal <github@vincentvanderwal.nl>
2025-08-19 12:30:40 +0200

    add the 2 warnings

--- src/routes/en/docs/cma-api/+page.svelte
+++ src/routes/en/docs/cma-api/+page.svelte
@@ -124,6 +124,29 @@
 	<link rel="canonical" href="https://open-meteo.com/en/docs/cma-api" />
 </svelte:head>
 
+<Alert.Root variant="warning" class="mb-4">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="lucide lucide-triangle-alert-icon lucide-triangle-alert"
+		><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3" /><path
+			d="M12 9v4"
+		/><path d="M12 17h.01" /></svg
+	>
+	<Alert.Description>
+		Due to recent high demand, CMA’s open-data services are currently experiencing download issues.
+		We hope these issues are resolved soon so the open-data community can once again access CMA
+		weather forecasts.
+	</Alert.Description>
+</Alert.Root>
+
 <Alert.Root variant="info" class="mb-4">
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
