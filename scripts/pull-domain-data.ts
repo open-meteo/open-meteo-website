83272cbde2cb569b89c8f9d415ffd2f234214def
Vincent van der Wal <info@vincentvanderwal.nl>
2025-11-13 11:37:54 +0100

    fetch domain data and parse for variables and labels

---
+++ scripts/pull-domain-data.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import { replaceInFileSync } from 'replace-in-file';
+
+export const pullDomainData = (generateNewOptionFiles = true) => {
+	return {
+		name: 'pull-domain-data',
+		transform(code, id, options) {
+			if (id.includes('src/routes/en/docs') && id.endsWith('options.ts')) {
+				if (options?.ssr) {
+					if (id.includes('dwd-api')) {
+						const basePath = id.replace('options.ts', '');
+						const domainMetaData = JSON.parse(
+							fs.readFileSync(basePath + 'meta-data-poc.json', 'utf8')
+						);
+
+						fetch('https://openmeteo.s3.amazonaws.com/data/dwd_icon/static/meta.json')
+							.then((response) => response.json())
+							.then(function (response) {
+								console.log(response);
+								let newHourly = `export const hourly = [
+  [`;
+								for (const [index, variable] of domainMetaData.variables.entries()) {
+									newHourly += `
+    { value: '${variable}', label: '${domainMetaData.labels[index]}' },`;
+								}
+
+								newHourly += `
+  ]
+];`;
+
+								const regex = /export const hourly = \[[\s\S]*?\];/;
+
+								if (generateNewOptionFiles) {
+									const replaceOptions = {
+										files: id,
+										from: regex,
+										to: newHourly,
+										countMatches: true
+									};
+
+									try {
+										const results = replaceInFileSync(replaceOptions);
+										console.log('Replacement results:', results);
+									} catch (error) {
+										console.error('Error occurred:', error);
+									}
+								} else {
+									const newCode = code.replace(regex, newHourly);
+									return newCode;
+								}
+							});
+					}
+				}
+			}
+		}
+	};
+};
--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,11 @@ import svg from '@poppanator/sveltekit-svg';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import fs from 'fs';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { type UserConfig, defineConfig } from 'vite';
 
 import rollupOptions from './rollup.config';
-
-const pullDomains = () => {
-	return {
-		name: 'pull-domain-data',
-		transform(code, id, options) {
-			if (id.includes('src/routes/en/docs') && id.endsWith('options.ts')) {
-				if (options?.ssr) {
-					if (id.includes('bom-api')) {
-						console.log(id);
-					}
-				}
-
-				// fs.appendFile(id, '//I should not be here', function (err) {
-				// 	if (err) throw err;
-				// 	console.log('Saved!');
-				// });
-			}
-		}
-	};
-};
+import { pullDomainData } from './scripts/pull-domain-data';
 
 const replaceChunckNames = () => {
 	return {
@@ -42,7 +22,7 @@ const replaceChunckNames = () => {
 
 export default defineConfig({
 	plugins: [
-		pullDomains(),
+		pullDomainData(),
 		tailwindcss(),
 		enhancedImages(),
 		sveltekit(),
