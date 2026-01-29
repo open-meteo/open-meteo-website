9a131ad222842123a2e6f4589a15160fc53ce52c
patrick-zippenfenig <patrick@zippenfenig.de>
2026-01-29 16:59:28 +0100

    naming

--- src/routes/en/docs/gfs-api/+page.svelte
+++ src/routes/en/docs/gfs-api/+page.svelte
@@ -1062,7 +1062,7 @@
 					<tr>
 						<th scope="row"
 							><a href="https://www.nco.ncep.noaa.gov/pmb/products/hgefs/" target="_blank"
-								>HGEFS 0.25° ens mean</a
+								>HGEFS 0.25° Ensemble Mean</a
 							></th
 						>
 						<td>Global</td>
