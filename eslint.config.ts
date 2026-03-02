f1b725c05f178c35c8206d101e94e82d154af12c
Vincent van der Wal <info@vincentvanderwal.nl>
2026-03-02 10:15:23 +0100

    new eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,38 +1,49 @@
-import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+import svelteConfig from './svelte.config.js';
 
-export default ts.config(
-	includeIgnoreFile(gitignorePath),
+export default defineConfig([
+	globalIgnores([
+		'node_modules',
+		'dist',
+		'build',
+		'coverage',
+		'.svelte-kit',
+		'.vscode',
+		'.gitignore'
+	]),
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
+	...svelte.configs.recommended,
 	prettier,
-	...svelte.configs['flat/prettier'],
+	...svelte.configs.prettier,
 	{
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off'
+		}
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
 			}
 		}
 	},
 	{
-		files: ['**/*.svelte'],
-
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
-		},
 		rules: {
-			'svelte/no-at-html-tags': 'off'
+			'svelte/no-navigation-without-resolve': 'off'
 		}
 	}
-);
+]);
