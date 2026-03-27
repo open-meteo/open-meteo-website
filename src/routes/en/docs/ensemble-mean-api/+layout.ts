588574962949b606b45525ef12567c8167c6ae6a
Vincent van der Wal <info@vincentvanderwal.nl>
2026-03-27 14:30:48 +0100

    bg position

--- src/routes/en/docs/ensemble-mean-api/+layout.ts
+++ src/routes/en/docs/ensemble-mean-api/+layout.ts
@@ -4,6 +4,7 @@ export const load = (() => {
 	return {
 		heroTitle: 'Ensemble Mean API',
 		heroDescription: 'Mean Ensemble Weather Forecasts from Multiple Models',
-		heroImage: '/images/backgrounds/forstberg.webp'
+		heroImage: '/images/backgrounds/forstberg.webp',
+		heroImagePosition: 'center 65%'
 	};
 }) satisfies LayoutLoad;
