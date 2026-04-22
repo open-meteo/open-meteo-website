12b420dfb609706b6a5dc31d3e667a7d170e8d5b
Vincent van der Wal <111272374+vincentvdwal@users.noreply.github.com>
2026-04-22 12:53:09 +0200

    feat: ensemble mean api page (#968)
    Co-authored-by: Patrick Zippenfenig <patrick@zippenfenig.de>

--- src/routes/en/docs/+layout.svelte
+++ src/routes/en/docs/+layout.svelte
@@ -37,7 +37,9 @@
 			]
 		},
 		{ title: 'Historical Weather', url: '/en/docs/historical-weather-api' },
-		{ title: 'Ensemble Models', url: '/en/docs/ensemble-api' },
+		{ title: 'Ensemble Models', url: '/en/docs/ensemble-api', children: [
+			{ title: 'Ensemble Mean API', url: '/en/docs/ensemble-mean-api' }
+		]},
 		{ title: 'Seasonal Forecast', url: '/en/docs/seasonal-forecast-api' },
 		{ title: 'Climate Change', url: '/en/docs/climate-api' },
 		{ title: 'Marine Forecast', url: '/en/docs/marine-weather-api' },
