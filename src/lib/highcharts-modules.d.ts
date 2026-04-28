8344d7161b90979c2097bae4b7c08a3e4995b0d7
Vincent van der Wal <github@vincentvanderwal.nl>
2026-04-28 15:48:43 +0200

    update ambient module def

--- src/lib/highcharts-modules.d.ts
+++ src/lib/highcharts-modules.d.ts
@@ -2,6 +2,6 @@
 // These are imported directly in highcharts-custom.ts for tree-shaking.
 declare module 'highcharts/es-modules/*' {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const value: any;
-	export default value;
+	const _: any;
+	export = _;
 }
