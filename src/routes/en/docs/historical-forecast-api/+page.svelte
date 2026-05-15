18e80fefc5dbfedb9420359611e893c217f9ab9d
patrick-zippenfenig <patrick@zippenfenig.de>
2026-05-15 15:12:06 +0200

    correct professional plan required single runs api

--- src/routes/en/docs/historical-forecast-api/+page.svelte
+++ src/routes/en/docs/historical-forecast-api/+page.svelte
@@ -853,7 +853,7 @@
 	</div>
 
 	<!-- LICENSE -->
-	<div class="mt-3 md:mt-6"><LicenceSelector /></div>
+	<div class="mt-3 md:mt-6"><LicenceSelector requires_professional_plan={true}/></div>
 </form>
 
 <!-- RESULTS -->
