e7e4941e6cf2a1bb15efc6c3b8a07256f82bce8b
Vincent van der Wal <111272374+vincentvdwal@users.noreply.github.com>
2026-07-01 08:57:51 +0200

    fix: provider prefix inconsistencies (#1103)

--- src/routes/en/docs/ensemble-api/options.ts
+++ src/routes/en/docs/ensemble-api/options.ts
@@ -429,7 +429,7 @@ export const ukmo_2km_variables = [
 	'is_day'
 ];
 
-export const gem_global_variables = [
+export const cmc_gem_global_variables = [
 	'temperature_2m',
 	'surface_pressure',
 	'snow_depth',
@@ -530,10 +530,10 @@ export const meteoswiss_variables = [
 ];
 
 export const availableVariables: Record<string, string[]> = {
-	icon_seamless_eps: icon_d2_variables,
-	icon_global_eps: icon_global_variables,
-	icon_eu_eps: icon_eu_variables,
-	icon_d2_eps: icon_d2_variables,
+	dwd_icon_seamless_eps: icon_d2_variables,
+	dwd_icon_global_eps: icon_global_variables,
+	dwd_icon_eu_eps: icon_eu_variables,
+	dwd_icon_d2_eps: icon_d2_variables,
 	ncep_gefs_seamless: gfs05_variables,
 	ncep_gefs025: gfs025_variables,
 	ncep_gefs05: gfs05_variables,
@@ -542,7 +542,7 @@ export const availableVariables: Record<string, string[]> = {
 	ecmwf_aifs025_ensemble: ecmwf_aifs025_variables,
 	ukmo_global_ensemble_20km: ukmo_variables,
 	ukmo_uk_ensemble_2km: ukmo_2km_variables,
-	gem_global_ensemble: gem_global_variables,
+	cmc_gem_geps: cmc_gem_global_variables,
 	bom_access_global_ensemble: bom_access_global_ensemble,
 	meteoswiss_icon_ch1_ensemble: meteoswiss_variables,
 	meteoswiss_icon_ch2_ensemble: meteoswiss_variables
@@ -696,10 +696,10 @@ export const solarVariables = [
 
 export const models = [
 	[
-		{ value: 'icon_seamless_eps', label: 'DWD ICON EPS Seamless' },
-		{ value: 'icon_global_eps', label: 'DWD ICON EPS Global' },
-		{ value: 'icon_eu_eps', label: 'DWD ICON EPS EU' },
-		{ value: 'icon_d2_eps', label: 'DWD ICON EPS D2' }
+		{ value: 'dwd_icon_seamless_eps', label: 'DWD ICON EPS Seamless' },
+		{ value: 'dwd_icon_global_eps', label: 'DWD ICON EPS Global' },
+		{ value: 'dwd_icon_eu_eps', label: 'DWD ICON EPS EU' },
+		{ value: 'dwd_icon_d2_eps', label: 'DWD ICON EPS D2' }
 	],
 	[
 		{ value: 'ncep_gefs_seamless', label: 'GFS Ensemble Seamless' },
@@ -710,7 +710,7 @@ export const models = [
 	[
 		{ value: 'ecmwf_ifs025_ensemble', label: 'ECMWF IFS 0.25° Ensemble' },
 		{ value: 'ecmwf_aifs025_ensemble', label: 'ECMWF AIFS 0.25° Ensemble' },
-		{ value: 'gem_global_ensemble', label: 'GEM Global Ensemble' },
+		{ value: 'cmc_gem_geps', label: 'GEM Global Ensemble' },
 		{ value: 'bom_access_global_ensemble', label: 'BOM ACCESS Global' }
 	],
 	[
