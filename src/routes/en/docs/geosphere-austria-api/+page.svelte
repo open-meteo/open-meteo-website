0b80f3530dcedc6bf8c1197b575dad455a4c72da
Terraputix <terraputix@mailbox.org>
2026-08-13 09:46:01 +0200

    fix: typo in documentation table (#1161)

--- src/routes/en/docs/geosphere-austria-api/+page.svelte
+++ src/routes/en/docs/geosphere-austria-api/+page.svelte
@@ -440,7 +440,7 @@
 				<thead>
 					<tr>
 						<th scope="col">Derived Variable</th>
-						<th scope="col">How it is derived?</th>
+						<th scope="col">How is it derived?</th>
 					</tr>
 				</thead>
 				<tbody>
